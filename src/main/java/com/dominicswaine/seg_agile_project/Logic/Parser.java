eec76fa0c5f9b7189be65b9b29c9fa25f85845a7
Horia Pavel <horiapavel@yahoo.com>
2018-12-02 14:28:23 +0000

    Implement tuz saving

--- src/main/java/com/dominicswaine/seg_agile_project/Logic/Parser.java
+++ src/main/java/com/dominicswaine/seg_agile_project/Logic/Parser.java
@@ -2,9 +2,14 @@ package com.dominicswaine.seg_agile_project.Logic;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class Parser {
 
@@ -25,15 +30,22 @@ public class Parser {
         JSONObject obj2 = new JSONObject();
         JSONArray players = new JSONArray();
 
+        // Tuzes
+        int whiteTuzIndex = board.getPlayerTuz(Side.WHITE);
+        int blackTuzIndex = board.getPlayerTuz(Side.BLACK);
 
         // Configuration for player '1'
 
-        obj1.put("name","player1");
+        obj1.put("name","white");
+
+        JSONObject whiteTuz = new JSONObject();
+        whiteTuz.put("tuz", whiteTuzIndex);
 
         JSONArray p1 = new JSONArray();
         JSONObject player1Kazan = new JSONObject();
         player1Kazan.put("kazan",kazans[0].getNumberOfKoorgools());
         p1.add(player1Kazan);
+        p1.add(whiteTuz);
         for(int i = 0 ; i < 9 ; ++i) {
             JSONObject player1Hole = new JSONObject();
             player1Hole.put("hole:" + i, holes[i].getNumberOfKoorgools() );
@@ -44,12 +56,17 @@ public class Parser {
 
         // Configuration for player '2'
 
-        obj2.put("name","player2");
+        obj2.put("name","black");
+
+
+        JSONObject blackTuz = new JSONObject();
+        blackTuz.put("tuz", blackTuzIndex);
 
         JSONArray p2 = new JSONArray();
         JSONObject player2Kazan = new JSONObject();
         player2Kazan.put("kazan",kazans[0].getNumberOfKoorgools());
         p2.add(player2Kazan);
+        p2.add(blackTuz);
         for(int i = 9 ; i < 18 ; ++i) {
             JSONObject player2Hole = new JSONObject();
             player2Hole.put("hole:" + (i - 9), holes[i].getNumberOfKoorgools() );
