80798fbbc840ea323d13f64bdca0ee8326bc310c
Horia Pavel <horiapavel@yahoo.com>
2018-12-02 11:02:00 +0000

    Add skeleton Parser class

---
+++ src/main/java/com/dominicswaine/seg_agile_project/Logic/Parser.java
@@ -0,0 +1,25 @@
+package com.dominicswaine.seg_agile_project.Logic;
+
+import org.json.simple.JSONObject;
+
+public class Parser {
+
+    private JSONObject obj;
+
+    public Parser() {
+        obj = new JSONObject();
+    }
+
+    public void addContent() {
+
+    }
+
+    public void writeToFile(String filePath) {
+
+    }
+
+    public void readFromFile(String filePath) {
+
+    }
+
+}
