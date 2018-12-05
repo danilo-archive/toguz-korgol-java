faf657535331b2e3fdcd4254630f6bea0e84641d
Horia Pavel <horiapavel@yahoo.com>
2018-12-05 11:24:49 +0000

    Update gradle and implement skeleton for ParserTest class

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ParserTest {
+
+    @Test
+    public void testTest() {
+        assertEquals(true,1==1);
+    }
+
+}
